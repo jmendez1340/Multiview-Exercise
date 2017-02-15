968fe288c9ee1a154bcdd2799cffa8a394d529aa
Jason Mendez <jasonmendez795@gmail.com>
2017-02-15 15:44:53 -0600

    initial commit!

---
+++ View Switcher/BlueViewController.swift
@@ -0,0 +1,45 @@
+//
+//  BlueViewController.swift
+//  View Switcher
+//
+//  Created by Jason Mendez on 2/13/17.
+//  Copyright © 2017 Jason Mendez. All rights reserved.
+//
+
+import UIKit
+
+class BlueViewController: UIViewController {
+    
+    @IBAction func blueButtonPressed(sender: UIButton) {
+        let alert = UIAlertController(title: "Blue View Button Pressed",
+                                      message: "You pressed the button on the blue view",
+                                      preferredStyle: .alert)
+        let action = UIAlertAction(title: "Yes, I did", style: .default,
+                                   handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true,completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
